f59c3d82ec7f3dc6ff8fe3fe2ee7432b6b9a49eb
Tobias Waslowski <tobi@Tobiass-MBP.fritz.box>
2023-11-11 17:18:16 +0100

    parameterize data visualisation for year/month

--- src/visualise.py
+++ src/visualise.py
@@ -1,54 +1,73 @@
-from pymongo import MongoClient
+import calendar
+from datetime import datetime
+
 import matplotlib.pyplot as plt
-from datetime import datetime, timedelta
 import pandas as pd
+from pymongo import MongoClient
 
-# Connect to MongoDB (replace with your connection details)
-client = MongoClient("mongodb://localhost:27017/")
-db = client.records
-collection = db.records
 
-# Filter records from the last calendar month
-start_date = datetime.now() - timedelta(days=30)
-query = {"timestamp": {"$gte": start_date.strftime('%Y-%m-%dT%H:%M:%S')}}
+def visualize_monthly_data(year: str = None, month: str = None):
+    # Connect to MongoDB (replace with your connection details)
+    client = MongoClient("mongodb://localhost:27017/")
+    db = client.records
+    collection = db.records
 
-# Retrieve data
-data = list(collection.find(query))
+    if year is None or month is None:
+        now = datetime.now()
+        year = now.year
+        month = now.month
 
-# Process data
-df = pd.DataFrame(data)
-df['timestamp'] = pd.to_datetime(df['timestamp']).dt.date
-df[['mood', 'sleep']] = df[['mood', 'sleep']].astype(float)
+    # Calculate the first and last day of the given month
+    first_day = datetime(year, month, 1)
+    last_day = datetime(year, month, calendar.monthrange(year, month)[1])
 
-# Calculate daily averages
-daily_avg = df.groupby('timestamp')[['mood', 'sleep']].mean().reset_index()
+    # Filter records for the specified month
+    query = {"timestamp": {"$gte": first_day.strftime('%Y-%m-%dT%H:%M:%S'),
+                           "$lte": last_day.strftime('%Y-%m-%dT%H:%M:%S')}}
 
-plt.style.use("seaborn-v0_8-dark")
+    # Retrieve data
+    data = list(collection.find(query))
 
-# Plotting
-plt.figure(figsize=(10, 6))
+    # Process data
+    df = pd.DataFrame(data)
+    df['timestamp'] = pd.to_datetime(df['timestamp']).dt.date
+    df[['mood', 'sleep']] = df[['mood', 'sleep']].astype(float)
 
-# Creating two y-axes
-ax1 = plt.gca()  # get current axes
-ax2 = ax1.twinx()  # create another axes that shares the same x-axis
+    # Calculate daily averages
+    daily_avg = df.groupby('timestamp')[['mood', 'sleep']].mean().reset_index()
 
-# Mood plot on ax1
-ax1.plot(daily_avg['timestamp'], daily_avg['mood'], marker='o', color='blue', markersize=3, label='Mood', linestyle='-')
-ax1.set_xlabel('Date')
-ax1.set_ylabel('Mood', color='blue')
-ax1.tick_params(axis='y', labelcolor='blue')
-ax1.set_ylim(-5, 5)
+    plt.style.use("seaborn-v0_8-dark")
 
-# Sleep plot on ax2
-ax2.plot(daily_avg['timestamp'], daily_avg['sleep'], marker='o', color='red', label='Sleep', markersize=3, linestyle='--')
-ax2.set_ylabel('Sleep', color='red')
-ax2.tick_params(axis='y', labelcolor='red')
+    # Plotting
+    plt.figure(figsize=(10, 6))
 
-# Title and layout
-plt.title('Average Mood and Sleep over the Last Month')
-plt.xticks(rotation=45)
-plt.tight_layout()
+    # Creating two y-axes
+    # Creating two y-axes
+    ax1 = plt.gca()
+    ax2 = ax1.twinx()
 
-# Save the plot as a JPG file
-plt.savefig('mood_sleep_combined_graph.jpg', format='jpg', dpi=300)
+    # Mood plot on ax1
+    ax1.plot(daily_avg['timestamp'], daily_avg['mood'], marker='o', color='blue', label='Mood', markersize=4,
+             linestyle='-')
+    ax1.set_xlabel('Date')
+    ax1.set_ylabel('Mood', color='blue')
+    ax1.tick_params(axis='y', labelcolor='blue')
+    ax1.set_ylim(-5, 5)
 
+    # Sleep plot on ax2
+    ax2.plot(daily_avg['timestamp'], daily_avg['sleep'], marker='o', color='green', label='Sleep', markersize=4,
+             linestyle='--')
+    ax2.set_ylabel('Sleep', color='green')
+    ax2.tick_params(axis='y', labelcolor='green')
+
+    # Title and layout
+    plt.title(f'Average Mood and Sleep for {month}/{year}')
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+
+    # Save the plot
+    plt.savefig(f'mood_sleep_{year}_{month}.jpg', format='jpg', dpi=300)
+
+
+if __name__ == '__main__':
+    visualize_monthly_data()
