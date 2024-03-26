cd75a5a8d4aae44903c1554f4c0a2406b89b6b8a
Tobias Waslowski <tobiaswaslowski@gmail.com>
2024-03-26 20:16:02 +0100

    create abstractrecord to not violate dry

--- src/model/record.py
+++ src/model/record.py
@@ -5,6 +5,15 @@ from pydantic import BaseModel
 from src.model.metric import Metric
 
 
+class RecordData(BaseModel):
+    """
+    Represents an instance of a metric, i.e. the metric name and a corresponding value.
+    """
+
+    metric_name: str
+    value: int | None
+
+
 class DatabaseRecord(BaseModel):
     """
     This exists for backwards-compatibility, as an abstraction layer to deal with my previous bad decisions.
@@ -24,43 +33,10 @@ class DatabaseRecord(BaseModel):
         )
 
 
-class RecordData(BaseModel):
+class AbstractRecord(BaseModel):
     """
-    Represents an instance of a metric, i.e. the metric name and a corresponding value.
+    Can provide shared methods for Record and TempRecord.
     """
-
-    metric_name: str
-    value: int | None
-
-
-class Record(BaseModel):
-    """
-    Record that is kept in the database. Backwards-compatible with the existing scheme via model_dump().
-    """
-
-    user_id: int
-    # this name is very unintuitive, as it overloads the meaning of the word "record"
-    # it is chosen like this for backward compatibility,
-    # but it should be migrated to something like 'data' or 'record_data' in the future
-    data: list[RecordData]
-    # gets serialized to an ISO 8601 string
-    timestamp: datetime
-
-    def find_data(self, name: str) -> RecordData | None:
-        return next((x for x in self.data if x.metric_name == name), None)
-
-
-# todo: usually I use pydantic, but here the post-init is easier to do with python's native dataclass
-# i don't think mixing them is a good idea; there's no necessity for a lot of data validation anyhow,
-# so maybe we can move away from pydantic entirely?
-class TempRecord(BaseModel):
-    """
-    Record that are kept in the in-memory ExpiringDict while being completed.
-    """
-
-    metrics: list[Metric]
-    data: list[RecordData] = None  # initialised in __init__
-    timestamp: datetime = datetime.now()
 
     # todo create tests
     def find_data(self, name: str) -> RecordData | None:
@@ -70,6 +46,23 @@ class TempRecord(BaseModel):
     def find_metric(self, user_prompt: str) -> Metric | None:
         return next((x for x in self.metrics if x.user_prompt == user_prompt), None)
 
+
+class Record(AbstractRecord):
+    user_id: int
+    data: list[RecordData]
+    timestamp: datetime
+
+
+class TempRecord(AbstractRecord):
+    """
+    Record that are kept in the in-memory ExpiringDict while being completed.
+    """
+
+    metrics: list[Metric]
+    data: list[RecordData] = None  # initialised in __init__
+    timestamp: datetime = datetime.now()
+
+    # todo test
     def update_data(self, name: str, value: int | None):
         record_data = self.find_data(name)
         if record_data:
