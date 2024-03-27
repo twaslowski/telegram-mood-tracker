ff9b0afd24ad0778456b1e7b224d945f94c6192d
Tobias Waslowski <tobias.waslowski@thoughtworks.com>
2024-03-27 14:15:28 +0100

    Add build instructions for armv8 image

--- scripts/docker-util.sh
+++ scripts/docker-util.sh
@@ -6,6 +6,24 @@ function build() {
   docker build -t mood-tracker:latest .
 }
 
+# using this for my raspberry pi
+function build_arm() {
+  mkdir -p package
+  poetry export -f requirements.txt -o package/requirements.txt --without-hashes
+  docker build -t mood-tracker:arm64v8 --platform arm64v8 .
+  docker tag mood-tracker:arm64v8 "public.ecr.aws/c1o1h8f4/mood-tracker:latest-arm64v8"
+  docker tag mood-tracker:arm64v8 "public.ecr.aws/c1o1h8f4/mood-tracker:${commit_sha}-arm64v8"
+}
+
+function push_arm() {
+    commit_sha=$(git rev-parse --short HEAD)
+    aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/c1o1h8f4
+    docker tag mood-tracker:arm64v8 "public.ecr.aws/c1o1h8f4/mood-tracker:latest-arm64v8"
+    docker tag mood-tracker:arm64v8 "public.ecr.aws/c1o1h8f4/mood-tracker:${commit_sha}-arm64v8"
+    docker push public.ecr.aws/c1o1h8f4/mood-tracker:latest-arm64v8
+    docker push "public.ecr.aws/c1o1h8f4/mood-tracker:${commit_sha}-arm64v8"
+}
+
 function push() {
   commit_sha=$(git rev-parse --short HEAD)
   aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/c1o1h8f4
