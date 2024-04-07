998631c2a6e1001c3f81c46451b81100de11f18a
Tobias Waslowski <tobias.waslowski@thoughtworks.com>
2024-04-07 18:03:17 +0200

    add clarifying comment to run script; remove log stream option

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+# This is how I personally choose to run the image.
+# I stream my logs to CloudWatch because it is convenient, and so I configure the log driver accordingly.
+# The --security-opt seccomp:unconfined flag is an unfortunate necessity because certain arm64 images do not run
+# properly on Raspberry Pis without it.
+# This is a known issue: https://github.com/distribution/distribution/issues/3008
+
 sudo docker run -d --rm \
   --name mood-tracker \
   -e TELEGRAM_TOKEN="$TELEGRAM_TOKEN" \
@@ -6,6 +14,5 @@ sudo docker run -d --rm \
   --log-driver=awslogs \
   --log-opt awslogs-region="$LOG_REGION" \
   --log-opt awslogs-group="$LOG_GROUP" \
-  --log-opt awslogs-stream="$LOG_STREAM" \
   --security-opt seccomp:unconfined \
   public.ecr.aws/c1o1h8f4/mood-tracker:latest
